afd4de30cfb413d5f447ac07de3e38a43189b939
Habra26 <arnaud.habraken@outlook.com>
2025-12-24 16:19:28 +0100

    Toggle menu hamburger

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
 // Automatisation de l'année dans le footer
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// Menu mobile
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+function closeMenu() {
+  mobileMenu.classList.add("hidden");
+}
+
+function toggleMenu() {
+  const isHidden = mobileMenu.classList.contains("hidden");
+
+  if (isHidden) {
+    mobileMenu.classList.remove("hidden");
+  } else {
+    closeMenu();
+  }
+}
+
+menuBtn.addEventListener("click", toggleMenu);
+
+// Fermer le menu quand on clique sur un lien
+mobileMenu.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+// Utilisation de Lucide icons
+lucide.createIcons();
